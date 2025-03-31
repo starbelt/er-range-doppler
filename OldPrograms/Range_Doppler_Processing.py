abede6f9f365fefe1ec403d8550992219c2bb44d
Nathan Griffin <nathangriffin@vt.edu>
2025-03-31 13:36:23 -0400

    testing mesh fix

--- OldPrograms/Range_Doppler_Processing.py
+++ OldPrograms/Range_Doppler_Processing.py
@@ -169,7 +169,7 @@ range_doppler_fig, ax = plt.subplots(1, figsize=(7,7))
 
 # Create proper mesh for accurate data placement
 velocity_mesh, range_mesh = np.meshgrid(dist, velocity_bins)
-range_doppler = ax.pcolormesh(range_mesh.T, velocity_mesh.T, raw_data, cmap=matplotlib.colormaps.get_cmap(cmn))
+range_doppler = ax.pcolormesh(velocity_mesh, range_mesh, raw_data, cmap=matplotlib.colormaps.get_cmap(cmn))
 
 # These limits can be set to display only part of the full range
 ax.set_xlim([-max_doppler_vel, max_doppler_vel])
